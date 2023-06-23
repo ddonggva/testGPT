ddac87fb138d13b27dd3ae13e498301ad94fb661
Dave Dong <dave.dong.gva@gmail.com>
2023-06-22 22:41:23 -0700

    adding langchain testing python code

--- testLangChain.py
+++ testLangChain.py
@@ -4,6 +4,8 @@ from langchain.callbacks.manager imort CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.agents import create_csv_agent
 
+# model download from here
+# https://huggingface.co/vihangd/open_llama_7b_700bt_ggml/tree/main
 
 
 
